893f496afe1028adbd95f3d9d3398cad3357dcbb
yun_sunjin <sunjin110@gmail.com>
2021-10-10 21:52:10 +0900

    day3

---
+++ build_kernel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source $HOME/osbook/devenv/buildenv.sh
+
+cd kernel
+clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp
+ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
+
+echo "--- kernel build finished ---"
